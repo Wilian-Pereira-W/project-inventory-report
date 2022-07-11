879c0e258a26fbf2ab2aa7bdc1c7544241cb564e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-07-11 18:04:01 -0300

    test: implementado o test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,34 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+import pytest
 
 
-def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+@pytest.fixture
+def product():
+    return Product(
+        1,
+        "Xbox Series x",
+        "Microsoft",
+        "2021-07-18",
+        "2035-10-05",
+        "123456789we",
+        "Qualquer lugar",
+    )
+
+
+def test_relatorio_produto(product):
+    print(repr(product))
+    assert (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    ) in repr(product)
+    assert (
+        "O produto Xbox Series x"
+        " fabricado em 2021-07-18"
+        " por Microsoft com validade"
+        " até 2035-10-05"
+        " precisa ser armazenado Qualquer lugar."
+    ) in repr(product)
+    assert ("validade") in repr(product)
