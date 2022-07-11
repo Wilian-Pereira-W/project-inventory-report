dc018bd8d4d1179a8a3f7b4689042810b2298f69
Wilian Pereira <wilian_pereira92@outlook.com>
2022-07-11 15:39:16 -0300

    test: implementado o test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+import pytest
+from inventory_report.inventory.product import Product
 
 
-def test_cria_produto():
-    pass  # Seu teste deve ser escrito aquii
+@pytest.fixture
+def product():
+    return Product(
+        1,
+        "Xbox Series x",
+        "Microsoft",
+        "2021-07-18",
+        "2035-10-05",
+        "123456789we",
+        "Qualquer lugar",
+    )
+
+
+def test_cria_produto(product):
+    assert product.id == 1
+    assert product.nome_do_produto == "Xbox Series x"
+    assert product.nome_da_empresa == "Microsoft"
+    assert product.data_de_fabricacao == "2021-07-18"
+    assert product.numero_de_serie == "123456789we"
+    assert product.instrucoes_de_armazenamento == "Qualquer lugar"
+    assert product.data_de_validade == "2035-10-05"
